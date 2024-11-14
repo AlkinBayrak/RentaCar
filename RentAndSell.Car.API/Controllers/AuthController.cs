69689b3ddbe020965b788f6ddde5c710d32eedc2
AlkinBayrak <alkinbayrak01@gmail.com>
2024-11-14 10:00:32 +0300

    Identity eklendi

---
+++ RentAndSell.Car.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentAndSell.Car.API.Models;
+
+namespace RentAndSell.Car.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		[HttpPost("Login")]
+		public ActionResult Login(LoginViewModel model)
+		{
+			LoginResultVİewModel loginResult = new LoginResultVİewModel();
+			loginResult.BasicAuth = "Basic abc123xyz";
+			loginResult.IsLogin = true;
+			loginResult.ErrorMessage = "";
+
+
+			return Ok(loginResult);
+		}
+	}
+}
---
+++ RentAndSell.Car.API/Models/LoginResultVİewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentAndSell.Car.API.Models
+{
+	public class LoginResultVİewModel
+	{
+        public string BasicAuth { get; set; }
+        public bool IsLogin { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
